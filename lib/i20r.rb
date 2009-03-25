f92ebd2ecd54b3ffbbbf1d75bb9ee1ce635c2c7c
Balint Erdi <balint.erdi@gmail.com>
2009-03-25 01:25:52 +0100

    adding replacement for Rails link_to helper
    Signed-off-by: Balint Erdi <balint.erdi@gmail.com>

--- lib/i20r.rb
+++ lib/i20r.rb
@@ -1,17 +1,17 @@
 require "rubygems"
 
 class I20r
-  
+
   def file_path_to_message_prefix(path)
     segments = path.split('/').select { |segment| !segment.empty? }
     file_name_without_extensions = segments.last.split('.').first
     segments.slice(2...-1).join('.') + '.' + file_name_without_extensions
   end
-  
+
   def get_i18n_message_string(text, prefix)
-    "#{prefix}.#{text.downcase}"
+    "#{prefix}.#{text.gsub(/\s/, '_').downcase}"
   end
-  
+
   def get_content_from(file)
     f = open(File.expand_path(file), "r")
     content = f.read()
@@ -24,18 +24,31 @@ class I20r
     f.write(new_content)
     f.close()
   end
-  
+
   def write_i18ned_file(file)
     text = get_content_from(file)
     prefix = file_path_to_message_prefix(file)
     i18ned_text = replace_non_i18_messages(text, prefix)
     write_content_to(file, i18ned_text)
   end
-  
+
+  def returning(value)
+    yield(value)
+    value
+  end
+
+  def replace_in_rails_helpers(text, prefix)
+    text.gsub(/<%=\s*link_to\s+['"](.*)['"]\s*/) do |match|
+      i18n_string = get_i18n_message_string($1, prefix)
+      %(<%= link_to I18n.t("#{i18n_string}"))
+    end
+  end
+
   def replace_non_i18_messages(text, prefix)
-    text.gsub(/>\s*(\w+)\s*</) do |match|
+    text = text.gsub(/>\s*(\w+)\s*</) do |match|
       i18n_string = get_i18n_message_string($1, prefix)
       %(><%= I18n.t("#{i18n_string}") %><)
     end
+    replace_in_rails_helpers(text, prefix)
   end
 end
--- spec/i20r_spec.rb
+++ spec/i20r_spec.rb
@@ -13,44 +13,60 @@ describe "I20r" do
     @i20r = I20r.new
   end
 
-  describe "message text replacement" do
-    before do
-      
-    end
+  it "should correctly turn file paths to message prefixes" do
+    @i20r.file_path_to_message_prefix("/app/views/users/new.html.erb").should == "users.new"
+  end
 
-    it "should correctly turn file paths to message prefixes" do
-      @i20r.file_path_to_message_prefix("/app/views/users/new.html.erb").should == "users.new"
-    end
-
-    it "should replace a plain text snippet with a message derived from the text and a prefix" do
+  describe "turning plain messages into i18n message strings" do
+    
+    it "should downcase a single word" do
       @i20r.get_i18n_message_string("Name", "users.new").should == "users.new.name"
     end
-    
-    describe "in files" do
-      before do
-        @file_path = '/app/views/users/new.html.erb'
-        message_prefix = "users.new"
-        @i20r.expects(:file_path_to_message_prefix).with(@file_path).returns(message_prefix)
 
-        @plain_snippet = <<-EOS
-          <label for="user-name">Name</label>
-          <input type="text" id="user-name" name="user[name]" />
-        EOS
-        @i18ned_snippet = <<-EOS
-          <label for="user-name"><%= I18n.t("#{message_prefix}.name") %></label>
-          <input type="text" id="user-name" name="user[name]" />
-        EOS
-
-        @i20r.expects(:get_content_from).with(@file_path).returns(@plain_snippet)
-        @i20r.expects(:write_content_to).with(@file_path, @i18ned_snippet).returns(true)
-      end
-      
-      it "should correctly replace plain texts with I18n-ed messages" do
-        # @i20r.replace_non_i18_messages(@plain_snippet).should == @i18ned_snippet
-        @i20r.write_i18ned_file(@file_path)
-      end
+    it "should replace spaces with underscores" do
+      @i20r.get_i18n_message_string("New name", "users.index").should == "users.index.new_name"
     end
+    
+  end
+  
+  describe "message text replacement" do
+    it "should replace words that are content in tags" do
+      plain = %(<label for="user-name">Name</label>)
+      i18ned = %(<label for="user-name"><%= I18n.t("users.new.name") %></label>)
+      @i20r.replace_non_i18_messages(plain, "users.new").should == i18ned        
+    end
+    
+    it "should replace a title in a link_to helper" do
+      plain = %(<p class="highlighted"><%= link_to 'New user', new_user_path %></p>)
+      i18ned = %(<p class="highlighted"><%= link_to I18n.t("users.index.new_user"), new_user_path %></p>)
+      @i20r.replace_non_i18_messages(plain, "users.index").should == i18ned
+    end
+    
+  end
 
+  describe "in files" do
+    before do
+      @file_path = '/app/views/users/new.html.erb'
+      message_prefix = "users.new"
+      @i20r.expects(:file_path_to_message_prefix).with(@file_path).returns(message_prefix)
+
+      @plain_snippet = <<-EOS
+        <label for="user-name">Name</label>
+        <input type="text" id="user-name" name="user[name]" />
+      EOS
+      @i18ned_snippet = <<-EOS
+        <label for="user-name"><%= I18n.t("#{message_prefix}.name") %></label>
+        <input type="text" id="user-name" name="user[name]" />
+      EOS
+
+      @i20r.expects(:get_content_from).with(@file_path).returns(@plain_snippet)
+      @i20r.expects(:write_content_to).with(@file_path, @i18ned_snippet).returns(true)
+    end
+    
+    it "should correctly replace plain texts with I18n-ed messages" do
+      # @i20r.replace_non_i18_messages(@plain_snippet).should == @i18ned_snippet
+      @i20r.write_i18ned_file(@file_path)
+    end
   end
 
 end
