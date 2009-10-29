f4fab57cc700ab8f07a2b0bfbba5a8a619182991
Balint Erdi <balint.erdi@gmail.com>
2009-10-29 18:15:59 +0100

    tag content matcher is migrated to the new style

--- lib/i15r/pattern_matcher.rb
+++ lib/i15r/pattern_matcher.rb
@@ -1,3 +1,4 @@
 require 'i15r/pattern_matchers/base'
 require 'i15r/pattern_matchers/tag_attribute_matcher'
 require 'i15r/pattern_matchers/rails_helper_matcher'
+require 'i15r/pattern_matchers/tag_content_matcher'
