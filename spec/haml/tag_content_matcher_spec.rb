2edb0a7944aba58118e01cd84e9afda016b08297
Balint Erdi <balint.erdi@gmail.com>
2009-12-14 18:28:23 +0100

    add a = for i18n rows

--- spec/haml/tag_content_matcher_spec.rb
+++ spec/haml/tag_content_matcher_spec.rb
@@ -3,7 +3,7 @@ require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 describe I15R::PatternMatchers::Haml::TagContentMatcher do
   it "should replace a tag's content where the tag is an implicit div" do
     plain = %(#form_head My account)
-    i18ned = %(#form_head I18n.t("users.edit.my_account"))
+    i18ned = %(#form_head= I18n.t("users.edit.my_account"))
     I15R::PatternMatchers::Haml::TagContentMatcher.run(plain, "users.edit").should == i18ned
   end
 
