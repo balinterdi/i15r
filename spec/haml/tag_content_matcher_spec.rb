58dd7a0e0ba49e5552c4c1d714a7e0d0b995ff0c
Balint Erdi <balint.erdi@gmail.com>
2010-11-28 14:23:04 +0100

    Fixes a couple of haml tag matchers

--- spec/haml/tag_content_matcher_spec.rb
+++ spec/haml/tag_content_matcher_spec.rb
@@ -11,7 +11,7 @@ describe I15R::PatternMatchers::Haml::TagContentMatcher do
 
   it "should replace a tag's content where the tag is an explicit one" do
     plain = %(%p Please check your inbox and click on the activation link.)
-    i18ned = %(%p I18n.t("users.show.please_check_your_inbox_and_click_on_the_activation_link"))
+    i18ned = %(%p= I18n.t("users.show.please_check_your_inbox_and_click_on_the_activation_link"))
     I15R::PatternMatchers::Haml::TagContentMatcher.run(plain, "users.show").should == i18ned
   end
 
@@ -25,7 +25,18 @@ describe I15R::PatternMatchers::Haml::TagContentMatcher do
     plain = %(        .field)
     I15R::PatternMatchers::Haml::TagContentMatcher.run(plain, "users.new").should == plain
   end
-  
+
+  it "should not replace a line with just an hmtl tag and no text" do
+    plain = "%p"
+    I15R::PatternMatchers::Haml::TagContentMatcher.run(plain, "users.new").should == plain
+  end
+
+  it "should not include the %tag in the generated message string" do
+    plain = "%h2 Resend unlock instructions"
+    i18ned = %(%h2= I18n.t("users.new.resend_unlock_instructions"))
+    I15R::PatternMatchers::Haml::TagContentMatcher.run(plain, "users.new").should == i18ned
+  end
+
   describe "when text has non-english characters" do
     it "should replace a tag's content where the tag is an implicit div" do
       plain = %(#form_head Türkçe)
@@ -35,10 +46,11 @@ describe I15R::PatternMatchers::Haml::TagContentMatcher do
 
     it "should replace a tag's content where the tag is an explicit one" do
       plain = %(%p Egy, kettő, három, négy, öt.)
-      i18ned = %(%p I18n.t("users.show.egy_kettő_három_négy_öt"))
+      i18ned = %(%p= I18n.t("users.show.egy_kettő_három_négy_öt"))
       I15R::PatternMatchers::Haml::TagContentMatcher.run(plain, "users.show").should == i18ned
     end
 
+  #1.8fail
     it "should replace a tag's content which is simple text all by itself on a line" do
       plain = %(Türkçe)
       i18ned = %(= I18n.t("users.new.türkçe"))
